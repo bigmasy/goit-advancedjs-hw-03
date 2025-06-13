f099592188fa0d34047a00b28c671591172f014d
bigmasy <jura.tymosh@gmail.com>
2025-06-13 13:10:59 +0300

    fix

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { onFormSubmit } from './js/pixabay-api.js';
 
 const refs = {
-  form: document.querySelector('.search'),
+  form: document.querySelector('.form'),
 };
 
 refs.form.addEventListener('submit', onFormSubmit);
